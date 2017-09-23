e3951f27ab24c18de311bd82663ac79c6d5dd35d
EuroWhisper <ralphjuden@gmail.com>
2017-09-24 03:53:45 +1300

    Basic hashtag detection added
    The Thought schema has been updated to include a hashTags attribute and hashtag detection has been added to thoughtsController.js.

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -10,15 +10,20 @@ exports.getAllThoughts = function(req, res) {
 // Create and save a new Thought object
 exports.createThought = function(req, res) {
 	console.log(req.body);
+	
+	// Detect hashtags.
+	// TODO: Modify REGEX so it only matches letters following the hash symbol, instead of also numbers and symbols.
+	let hashTags = req.body.thought.match(/#(?:\[[^\]]+\]|\S+)/g);
+	
+	
 	// Create a new Thought object from the POST data.
-	
-	// Store a collection of hashtags detected from within req.body.thought
-	let hashTags;
-	
 	var thought = new Thought({
 		name: req.body.name,
-		thought: req.body.thought
+		thought: req.body.thought,
+		hashTags: hashTags
 	});
+	
+	console.log(thought.hashtags);
 	
 	// Save the new thought.
 	thought.save(function(err) {
